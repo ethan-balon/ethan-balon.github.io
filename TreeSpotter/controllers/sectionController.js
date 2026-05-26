38aa0139dfe41b98560d93e1e451e6d793cbfc22
Ethan Balon [ebb0039] <ebb0039@arastudent.ac.nz>
2026-05-27 11:18:27 +1200

    Finished sections code and css structure

--- TreeSpotter/controllers/sectionController.js
+++ TreeSpotter/controllers/sectionController.js
@@ -4,8 +4,11 @@ import { SectionView } from "../views/sectionView.js";
 class sectionController {
     constructor() {
         this.SectionView = new SectionView()
+        const introductionChecker = localStorage.getItem("introductionChecker")
+        this.introductionChecker = introductionChecker
     }
 
+    //replace the current page with the required page
     refresh_page(pageName) {
     const sections = document.querySelectorAll(".section")
         for (let i=0; i<sections.length; i++){
@@ -17,8 +20,9 @@ class sectionController {
         }   
     }
 
+    //enables functionality of section buttons
     bindSection() {
-        const [mapButton, tutorialButton, savedListButton, aboutButton, advancedOptionsButton] = this.SectionView.get_values()
+        const [mapButton, tutorialButton, savedListButton, aboutButton, advancedOptionsButton, introductionClose, introductionDSA, introductionTutorial] = this.SectionView.get_values()
 
         mapButton.onclick = ()=> {
             this.refresh_page('map_page')
@@ -39,9 +43,35 @@ class sectionController {
         advancedOptionsButton.onclick = ()=> {
             this.refresh_page('advanced_options_page')
         }
+
+
+        //BUTTONS ON INTRODUCTION PAGE
+        introductionClose.onclick = ()=> {
+            this.refresh_page('map_page')
+        }
         
+        introductionDSA.onclick = ()=> {
+            localStorage.setItem("introductionChecker", true)
+            this.refresh_page('map_page')
+        }
+
+        introductionTutorial.onclick = ()=> {
+            this.refresh_page('tutorial_page')
+        }
+    }
+
+
+
+    //determines what should be the initial page displayed to the user depending on previous settings
+    intialPage() {
+        if (this.introductionChecker) {
+            this.refresh_page('map_page')
+        }else{
+            this.refresh_page('introduction_page')
+        }
     }
 }
 
 const app = new sectionController()
 app.bindSection()
+app.intialPage()
--- TreeSpotter/views/sectionView.js
+++ TreeSpotter/views/sectionView.js
@@ -5,6 +5,9 @@ export class SectionView {
         let saved_list_button = document.getElementById('saved_list_button')
         let about_button = document.getElementById('about_button')
         let advanced_options_button = document.getElementById('advanced_options_button')
-        return [map_button, tutorial_button, saved_list_button, about_button, advanced_options_button]
+        let introduction_close = document.getElementById('introduction_close')
+        let introduction_dsa = document.getElementById('introduction_dsa')
+        let introduction_tutorial = document.getElementById('introduction_tutorial')
+        return [map_button, tutorial_button, saved_list_button, about_button, advanced_options_button, introduction_close, introduction_dsa, introduction_tutorial]
     }
 }
